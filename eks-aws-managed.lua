26de2af59f3dea47becad36157dab246b53cbf26
grey1001 <130705843+grey1001@users.noreply.github.com>
2023-07-19 07:36:49 +0200

    Add files via upload

---
+++ eks-aws-managed.lua
@@ -0,0 +1,39 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = "vpc-072870e7d535cd848"
+  subnet_ids               = ["subnet-049b4457678c3d074", "subnet-04169a7eff9cc9383", "subnet-067c805132b51c988"]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t2.micro"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      instance_types = ["t2.micro"]
+    }
+  }
+}
